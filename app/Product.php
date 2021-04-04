bf5caf6fd9269166b10775e4b753148a07536c07
Falldanger <bohdan.murga@yandex.ua>
2021-04-04 20:42:02 +0300

    Added admin product resource and views

--- app/Product.php
+++ app/Product.php
@@ -4,15 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ * @package App
+ */
 class Product extends Model
 {
-    protected $fillable = ['name', 'code'];
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['name', 'code', 'price', 'category_id', 'description', 'image'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
+    /**
+     * @return float|int|mixed
+     */
     public function getPriceForCount()
     {
         if (!is_null($this->pivot)) {
