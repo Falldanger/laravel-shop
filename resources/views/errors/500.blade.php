5bc2a79c555377bd08b504fc0e7462f294413c8c
Falldanger <bohdan.murga@yandex.ua>
2021-04-12 20:09:07 +0300

    Added Debagbar package. Fixed N+1 problem in MainController index function. Added view to 500 server error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,23 @@
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>500 Server error</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">
+
+    <link type="text/css" rel="stylesheet" href="/css/500.css">
+</head>
+<body>
+<div id="notfound">
+    <div class="notfound">
+        <div class="notfound-404">
+            <h1>Oops!</h1>
+            <h2>500 - Server error</h2>
+        </div>
+        <a href="{{route('index')}}">Go TO Homepage</a>
+    </div>
+</div>
+</body>
+</html>
