4a95d342fbb884622303220dd328337fb982494e
Falldanger <bohdan.murga@yandex.ua>
2021-04-13 23:21:35 +0300

    Added scopes to product model

--- app/Product.php
+++ app/Product.php
@@ -81,4 +81,19 @@ class Product extends Model
     {
         return $this->recommend === 1;
     }
+
+    public function scopeHit($query)
+    {
+        return $query->where('hit', 1);
+    }
+
+    public function scopeNew($query)
+    {
+        return $query->where('new', 1);
+    }
+
+    public function scopeRecommend($query)
+    {
+        return $query->where('recommend', 1);
+    }
 }
