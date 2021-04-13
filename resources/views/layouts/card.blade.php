db9b56c53c6123ac4f3e32bbb2d6d85281ad0112
Falldanger <bohdan.murga@yandex.ua>
2021-04-13 23:11:50 +0300

    Fixed call of N+1 products in card.blade. Fixed Products fileter by price

--- resources/views/layouts/card.blade.php
+++ resources/views/layouts/card.blade.php
@@ -13,14 +13,15 @@
                 <span class="badge badge-danger">Хит продаж!</span>
             @endif
         </div>
-        <img src="{{ Storage::url($product->image) }}" alt="iPhone X 64GB">
+        <img src="{{ Storage::url($product->image) }}" alt="">
         <div class="caption">
             <h3>{{ $product->name }}</h3>
             <p>{{ $product->price }} руб.</p>
             <p>
             <form action="{{ route('basket-add', $product) }}" method="POST">
                 <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                <a href="{{ route('product', [$product->category->code, $product->code]) }}" class="btn btn-default"
+                <a href="{{ route('product', [isset($category)?$category->code:$product->category->code, $product->code]) }}"
+                   class="btn btn-default"
                    role="button">Подробнее</a>
                 @csrf
             </form>
