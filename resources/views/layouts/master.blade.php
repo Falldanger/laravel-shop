40a39d976ca96b823e711232ab85d3a076f680ed
Falldanger <bohdan.murga@yandex.ua>
2021-04-21 20:32:18 +0300

    Deleted spare letter on navbar panel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,7 +37,7 @@
             <ul class="nav navbar-nav navbar-right">
                 @guest
                     <li><a href="{{ route('login') }}">@lang('main.login')</a></li>
-                @endguestцу
+                @endguest
 
                 @auth
                     @admin
