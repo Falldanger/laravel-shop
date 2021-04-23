8383a4717b9f249342090fdf0ef14844d999c67f
Falldanger <bohdan.murga@yandex.ua>
2021-04-23 23:47:49 +0300

    Added currency session and all currencies from db to master blade view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SubscriptionRequest;
 use App\Models\Category;
+use App\Models\Currency;
 use App\Models\Product;
 use App\Http\Requests\ProductsFilterRequest;
 use App\Models\Subscription;
@@ -72,6 +73,13 @@ class MainController extends Controller
         App::setLocale($locale);
         $currentLocale = App::getLocale();
 
+        return redirect()->back();
+    }
+
+    public function changeCurrency($currencyCode)
+    {
+        $currency = Currency::byCode($currencyCode)->firstOrFail();
+        session(['currency' => $currency->code]);
         return redirect()->back();
     }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,10 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>
-        @lang('main.online_shop'):
-        @yield('title')
-    </title>
+    <title>@lang('main.online_shop'): @yield('title')</title>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <script src="/js/jquery.min.js"></script>
@@ -20,18 +17,33 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="{{route('index')}}">@lang('main.online_shop')</a>
+            <a class="navbar-brand" href="{{ route('index') }}">@lang('main.online_shop')</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li @routeactive('index')><a href="{{route('index')}}">@lang('main.all_products')</a></li>
-                <li @routeactive('categor*')><a href="{{route('categories')}}">@lang('main.categories')</a>
+                <li @routeactive('index')><a href="{{ route('index') }}">@lang('main.all_products')</a></li>
+                <li @routeactive('categor*')><a href="{{ route('categories') }}">@lang('main.categories')</a>
                 </li>
-                <li @routeactive('basket*')><a href="{{route('basket')}}">@lang('main.cart')</a></li>
+                <li @routeactive('basket*')><a href="{{ route('basket') }}">@lang('main.cart')</a></li>
                 <li><a href="{{ route('reset') }}">@lang('main.reset_project')</a></li>
-            <li>
-                <a href="{{route('locale',__('main.set_lang'))}}">@lang('main.set_lang')</a>
-            </li>
+                <li><a href="{{ route('locale', __('main.set_lang')) }}">@lang('main.set_lang')</a></li>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">
+                        Currency
+{{--                        {{ App\Services\CurrencyConversion::getCurrencySymbol() }}--}}
+                        <span
+                            class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        @foreach(App\Models\Currency::get() as $currency)
+                            <li><a href="">{{ $currency->symbol }}</a></li>
+                        @endforeach
+                        {{--                        @foreach (App\Services\CurrencyConversion::getCurrencies() as $currency)--}}
+                        {{--                            <li><a href="{{ route('currency', $currency->code) }}">{{ $currency->symbol }}</a></li>--}}
+                        {{--                        @endforeach--}}
+                    </ul>
+                </li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
@@ -55,10 +67,10 @@
 <div class="container">
     <div class="starter-template">
         @if(session()->has('success'))
-            <p class="alert alert-success">{{session()->get('success')}}</p>
+            <p class="alert alert-success">{{ session()->get('success') }}</p>
         @endif
         @if(session()->has('warning'))
-            <p class="alert alert-warning">{{session()->get('warning')}}</p>
+            <p class="alert alert-warning">{{ session()->get('warning') }}</p>
         @endif
         @yield('content')
     </div>
